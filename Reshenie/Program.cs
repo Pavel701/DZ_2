e5136744613c8df925f3dc79e1600860ac1eb548
Pavel Sh <paulshipspb@gmail.com>
2023-04-17 19:29:43 +0300

    Решение третей задачи

--- Reshenie/Program.cs
+++ Reshenie/Program.cs
@@ -11,25 +11,37 @@
     //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
     //645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
     
-Console.Write("Введите трёхзначное число: ");
-int number = int.Parse(Console.ReadLine());
-// Если число меньше 100 то третьей цифры нет
-if (number < 100) 
+// Console.Write("Введите трёхзначное число: ");
+// int number = int.Parse(Console.ReadLine());
+// // Если число меньше 100 то третьей цифры нет
+// if (number < 100) 
+// {
+// Console.WriteLine("Третьей цифры нет.");
+// } 
+// else if (number > 999)
+// {
+// // Вычисляем третью цифру числа
+// int result = (number / 100) % 10;
+// Console.WriteLine("Третья цифра числа: " + result);
+// }
+// else 
+// {
+// // Вычисляем третью цифру числа
+// int result = (number / 1) % 10;
+// Console.WriteLine("Третья цифра числа: " + result);
+// }
+
+
+  //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да, 7 -> да, 1 -> нет
+Console.Write("Введите цифру дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7) 
 {
-Console.WriteLine("Третьей цифры нет.");
+Console.WriteLine("Это выходной день.");
 } 
-else if (number > 999)
-{
-// Вычисляем третью цифру числа
-int result = (number / 100) % 10;
-Console.WriteLine("Третья цифра числа: " + result);
-}
 else 
 {
-// Вычисляем третью цифру числа
-int result = (number / 1) % 10;
-Console.WriteLine("Третья цифра числа: " + result);
+Console.WriteLine("Этот невыходной день.");
 }
 
-
-  
