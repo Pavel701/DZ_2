6931091e4f76826065bdbe75629a04c8f6b1881b
Pavel Sh <paulshipspb@gmail.com>
2023-04-17 19:35:58 +0300

    Решение четвёртой задачи

--- Reshenie/Program.cs
+++ Reshenie/Program.cs
@@ -34,14 +34,23 @@
 
   //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 //6 -> да, 7 -> да, 1 -> нет
-Console.Write("Введите цифру дня недели: ");
-int day = int.Parse(Console.ReadLine());
-if (day == 6 || day == 7) 
-{
-Console.WriteLine("Это выходной день.");
-} 
-else 
-{
-Console.WriteLine("Этот невыходной день.");
-}
+// Console.Write("Введите цифру дня недели: ");
+// int day = int.Parse(Console.ReadLine());
+// if (day == 6 || day == 7) 
+// {
+// Console.WriteLine("Это выходной день.");
+// } 
+// else 
+// {
+// Console.WriteLine("Этот невыходной день.");
+// }
+
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1
+
+    Console.Write("Введите трёхзначное число: ");
+    int number = int.Parse(Console.ReadLine());
+    int secondDigit = (number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
 
