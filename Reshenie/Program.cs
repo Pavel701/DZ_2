077a5db413b0c30585120c376d9545192e63fad4
Pavel Sh <paulshipspb@gmail.com>
2023-04-17 17:52:38 +0300

    Решение второй задачи

--- Reshenie/Program.cs
+++ Reshenie/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1
+
+    // Console.Write("Введите трёхзначное число: ");
+    // int number = int.Parse(Console.ReadLine());
+    // int secondDigit = (number / 10) % 10;
+    // Console.WriteLine($"Вторая цифра числа {number} равна {secondDigit}");
+  
+
+
+    //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    //645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+    
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+// Если число меньше 100 то третьей цифры нет
+if (number < 100) 
+{
+Console.WriteLine("Третьей цифры нет.");
+} 
+else if (number > 999)
+{
+// Вычисляем третью цифру числа
+int result = (number / 100) % 10;
+Console.WriteLine("Третья цифра числа: " + result);
+}
+else 
+{
+// Вычисляем третью цифру числа
+int result = (number / 1) % 10;
+Console.WriteLine("Третья цифра числа: " + result);
+}
+
+
+  
